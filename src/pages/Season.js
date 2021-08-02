82a96f05bcad887ad3cd7449320fe19a4dde4e49
TauDuque <hectauduque@gmail.com>
2021-08-02 16:17:43 -0300

    final

--- src/pages/Season.js
+++ src/pages/Season.js
@@ -36,8 +36,6 @@ const Season = () => {
   const { temporada, plot, hero_img, main_plot, third_plot, final, images } =
     single_season;
 
-  console.log(plot, main_plot, third_plot);
-
   if (single_season) {
     return (
       <Wrapper>
@@ -52,41 +50,13 @@ const Season = () => {
               <img src={images && images[0].image} alt="main" />
             </div>
             <div className="first-text">
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex
-                quia molestiae ea cum nemo quaerat tempora esse quasi? Culpa non
-                hic eum cupiditate, possimus numquam fugiat beatae repellat,
-                dicta nisi itaque alias provident exercitationem iusto. Non
-                corrupti consectetur quisquam? Nihil voluptas distinctio cum
-                cumque animi? Lorem, ipsum dolor sit amet consectetur
-                adipisicing elit. Ex quia molestiae ea cum nemo quaerat tempora
-                esse quasi? Culpa non hic eum cupiditate, possimus numquam
-                fugiat beatae repellat, dicta nisi itaque alias provident
-                exercitationem iusto. Non corrupti consectetur quisquam? Nihil
-                voluptas distinctio cum cumque animi?
-              </p>
+              <p>{plot}</p>
             </div>
           </div>
           <div className="second-col">
             <div className="text-container">
               <img src={omar_cartoon} alt="Omar" />
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere
-                tenetur ex voluptate harum, hic facilis ipsa temporibus vel quas
-                sunt cumque sapiente magnam perferendis, soluta sed illum quasi
-                iste minima? Ipsam corrupti saepe eveniet magni aspernatur atque
-                sunt laborum necessitatibus nisi sit, nemo deserunt veniam
-                consequuntur eos, ea dolore. Vel nemo quod inventore accusantium
-                corporis ad, quia harum corrupti unde dolore ipsum cum nobis
-                laboriosam numquam consequuntur nostrum deserunt dolor optio
-                ipsa neque saepe iusto magnam ullam ex. Ducimus culpa, est,
-                officia architecto consectetur harum dolore sequi molestiae
-                eligendi recusandae quas accusamus dolorem tempore deserunt,
-                sunt fugiat. Perferendis vitae, expedita esse doloribus sed
-                porro amet ipsa, doloremque nesciunt reiciendis distinctio eos
-                excepturi dolorum quaerat quisquam voluptas nemo quo voluptatum
-                dignissimos..
-              </p>
+              <p>{main_plot}</p>
               <img
                 src={string_avon}
                 alt="icon"
@@ -99,19 +69,7 @@ const Season = () => {
           </div>
           <div className="fourth-col">
             <div className="second-text">
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex
-                quia molestiae ea cum nemo quaerat tempora esse quasi? Culpa non
-                hic eum cupiditate, possimus numquam fugiat beatae repellat,
-                dicta nisi itaque alias provident exercitationem iusto. Non
-                corrupti consectetur quisquam? Nihil voluptas distinctio cum
-                cumque animi? Lorem, ipsum dolor sit amet consectetur
-                adipisicing elit. Ex quia molestiae ea cum nemo quaerat tempora
-                esse quasi? Culpa non hic eum cupiditate, possimus numquam
-                fugiat beatae repellat, dicta nisi itaque alias provident
-                exercitationem iusto. Non corrupti consectetur quisquam? Nihil
-                voluptas distinctio cum cumque animi?
-              </p>
+              <p>{third_plot}</p>
             </div>
             <div className="second-img">
               <img src={images && images[1].image} alt="main" />
